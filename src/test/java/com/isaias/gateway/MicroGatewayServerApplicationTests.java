d1e9f5449ef53046525bedff67c762f80416a2cf
IsaiasMorochi <imorochi.dev@gmail.com>
2022-04-26 10:02:13 -0400

    Configuracion Spring Cloud Gateway

---
+++ src/test/java/com/isaias/gateway/MicroGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaias.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
